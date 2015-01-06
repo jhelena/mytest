4f61a5ac948bbfb66d26cddf3a5ddb561df99a97
jhelena <jhelena@yandex.ru>
2015-01-06 23:49:55 +0300

    OK commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+echo "<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<title>Open Server</title>
+</head>
+<body style=\"background: url(fon.png) top left repeat-x\">
+<center>
+<br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana; text-shadow: 0 1px 0 #fff\">Добро пожаловать на мой склонированный сайт!</span>
+<br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Он работает ;-)</span>
+<br><br></div>
+</center>
+</body>
+</html>";
+?>
